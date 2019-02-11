6803f3069748062707025caab27778832230aadb
table <sri9132@gmail.com>
2019-02-11 14:46:03 +0530

    First commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"http://rapidtest.ga/foodieschoice/mobileapp/api",	
+	'DialogDefaultTitle':"FC",	
+	'pushNotificationSenderid':"",	
+	'facebookAppId':"", 
+	'APIHasKey':"69d10a57daa34341c6fd17d047a8c2b7"
+};
