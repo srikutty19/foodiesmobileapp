5dd3b0451ae1edf9a4635d3a5da512dd87d38b47
srikutty19 <sri9132@gmail.com>
2019-02-11 18:14:16 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
 	'ApiUrl':"http://rapidtest.ga/foodieschoice/mobileapp/api",	
 	'DialogDefaultTitle':"FC",	
-	'pushNotificationSenderid':"",	
+	'pushNotificationSenderid':"AIzaSyCTsU7jCaFDGUnae5ZnFk12R_Vs-_r_EpI",	
 	'facebookAppId':"", 
 	'APIHasKey':"69d10a57daa34341c6fd17d047a8c2b7"
 };
